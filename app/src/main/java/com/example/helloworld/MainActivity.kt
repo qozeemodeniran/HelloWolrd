70d852c362e97537699983ec16c9e60b1c99d591
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-04-20 17:02:16 +0100

    finished app functionalities

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    // variable declaration for counting numbers
+    var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Function definition(as refrence in activity_main.xml)
+    fun increase(view : View){
+        // increment operation
+        counter++
+        // Getting updated value of the counter on tex-view
+        val textView = findViewById(R.id.count) as TextView
+        textView.text =  "$counter"
     }
 }
